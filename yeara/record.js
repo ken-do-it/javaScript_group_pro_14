8ca8bdc4397a910f303be663f6fc383c8ea6cb3d
kijin lee <lqizhen@naver.com>
2024-07-21 13:18:12 +1000

    hamburger dispaly2

--- yeara/record.js
+++ yeara/record.js
@@ -1,13 +1,13 @@
 // 햄버거 메뉴 열기 함수
 function openMenu() {
     document.getElementById("slideMenu").style.width = "250px";
-    // document.getElementById("menuIcon").style.display = "none"; // 아이콘 숨기기
+    document.getElementById("menuIcon").style.display = "none"; // 아이콘 숨기기
 }
 
 // 햄버거 메뉴 닫기 함수
 function closeMenu() {
     document.getElementById("slideMenu").style.width = "0";
-    // document.getElementById("menuIcon").style.display = "block"; // 아이콘 보이기
+    document.getElementById("menuIcon").style.display = "block"; // 아이콘 보이기
 }
 
 // 슬라이드 메뉴 링크 클릭 시 메뉴 닫기
@@ -16,6 +16,14 @@ document.querySelectorAll('.slide-menu a').forEach(item => {
         closeMenu();
     });
 });
+
+
+
+
+
+
+
+
 
 // DOMContentLoaded 이벤트 핸들러
 document.addEventListener('DOMContentLoaded', function() {
