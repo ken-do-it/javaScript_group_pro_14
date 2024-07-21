3577ed678299c25b465f9b5b7b5673c8b9b9539a
kijin lee <lqizhen@naver.com>
2024-07-21 13:03:59 +1000

    hamburger display

--- yeara/record.js
+++ yeara/record.js
@@ -1,11 +1,30 @@
+// 햄버거 메뉴 열기 함수
+function openMenu() {
+    document.getElementById("slideMenu").style.width = "250px";
+    // document.getElementById("menuIcon").style.display = "none"; // 아이콘 숨기기
+}
 
+// 햄버거 메뉴 닫기 함수
+function closeMenu() {
+    document.getElementById("slideMenu").style.width = "0";
+    // document.getElementById("menuIcon").style.display = "block"; // 아이콘 보이기
+}
+
+// 슬라이드 메뉴 링크 클릭 시 메뉴 닫기
+document.querySelectorAll('.slide-menu a').forEach(item => {
+    item.addEventListener('click', event => {
+        closeMenu();
+    });
+});
+
+// DOMContentLoaded 이벤트 핸들러
 document.addEventListener('DOMContentLoaded', function() {
     let caloriesBurned = localStorage.getItem('caloriesBurned');
     let workoutTime = localStorage.getItem('workoutTime');
     
     if (caloriesBurned) {
-        caloriesBurned = parseFloat(caloriesBurned).toFixed(0);//소수점 반올림
-        document.getElementById('caloriesBurned').innerHTML = `<i class="fa-solid fa-fire" style=color:red;></i> Burn calories: ${caloriesBurned} kcal`;
+        caloriesBurned = parseFloat(caloriesBurned).toFixed(0); // 소수점 반올림
+        document.getElementById('caloriesBurned').innerHTML = `<i class="fa-solid fa-fire" style="color:red;"></i> Burn calories: ${caloriesBurned} kcal`;
     } else {
         document.getElementById('caloriesBurned').textContent = 'Burn calories: 0 kcal';
     }
@@ -17,20 +36,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-// side menu hamburger
-function openMenu() {
-    document.getElementById("slideMenu").style.width = "250px";
-    document.getElementById("menuIcon").style.display = "none"; // 아이콘 숨기기
-}
-
-function closeMenu() {
-    document.getElementById("slideMenu").style.width = "0";
-    document.getElementById("menuIcon").style.display = "block"; // 아이콘 보이기
-}
-
-document.querySelectorAll('.slide-menu a').forEach(item => {
-    item.addEventListener('click', event => {
-        closeMenu();
-    });
-});
-
+let logs = JSON.parse(localStorage.getItem('foodLogs')) || {};
